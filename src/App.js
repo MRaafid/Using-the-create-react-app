efd5be45cfafaa217b9e9668f6703f2e540c3b0c
zero <muhammadraafid50290@gmail.com>
2024-02-15 22:21:09 +0530

    Last Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+import React, {Component} from 'react';
+class MyApp extends Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        userInput: '',
+        reversedText: ''
+      };
+    }
+  
+    handleInputChange = (event) => {
+      const userInput = event.target.value;
+      const reversedText = userInput.split('').reverse().join(''); // Reverse the input text
+      this.setState({ userInput, reversedText });
+    };
+  
+    render() {
+      return (
+        <div id="app">
+          <h1>Reverse Text</h1>
+          <input
+            type="text"
+            value={this.state.userInput}
+            onChange={this.handleInputChange}
+            placeholder="Type something..."
+          />
+          <p>Reversed Text: {this.state.reversedText}</p>
+        </div>
+      );
+    }
+  }
+  
+  export default MyApp;
